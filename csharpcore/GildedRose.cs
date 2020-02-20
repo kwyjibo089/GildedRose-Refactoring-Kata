53f07a1fd964b8dc39339ecd108a17317df11692
roman <roman.mueller@swisslife.ch>
2020-02-20 14:36:49 +0100

    refactored code

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -14,24 +14,28 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                switch (Items[i].Name)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+                    case "Aged Brie":
+                        if (Items[i].Quality < 50)
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            Items[i].Quality = Items[i].Quality + 1;
                         }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        Items[i].SellIn = Items[i].SellIn - 1;
+                        if (Items[i].SellIn < 0 && Items[i].Quality < 50)
                         {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        Items[i].SellIn = Items[i].SellIn - 1;
+
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+
                             if (Items[i].SellIn < 11)
                             {
                                 if (Items[i].Quality < 50)
@@ -47,42 +51,27 @@ namespace csharpcore
                                     Items[i].Quality = Items[i].Quality + 1;
                                 }
                             }
-                        }
-                    }
-                }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
+                            if (Items[i].SellIn < 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                Items[i].Quality = 0;
                             }
                         }
-                        else
+                        break;
+                    default:
+                        Items[i].SellIn = Items[i].SellIn - 1;
+                        
+                        if (Items[i].Quality > 0)
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality = Items[i].Quality - 1;
+                            if (Items[i].SellIn < 0 && Items[i].Quality > 0)
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
                         }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+
+                        break;
+                }               
             }
         }
     }
