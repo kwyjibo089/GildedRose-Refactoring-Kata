2e515c03c6df8c4a472dc7dac3933586a531cbfe
roman <roman.mueller@swisslife.ch>
2020-02-20 14:45:35 +0100

    refactored

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -14,24 +14,26 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                if(Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    continue;
+                }
+
+                Items[i].SellIn = Items[i].SellIn - 1;
+
                 switch (Items[i].Name)
                 {
-                    case "Sulfuras, Hand of Ragnaros":
-                        break;
                     case "Aged Brie":
                         if (Items[i].Quality < 50)
                         {
                             Items[i].Quality = Items[i].Quality + 1;
-                        }
-                        Items[i].SellIn = Items[i].SellIn - 1;
-                        if (Items[i].SellIn < 0 && Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                            if (Items[i].SellIn < 0)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }                        
                         break;
                     case "Backstage passes to a TAFKAL80ETC concert":
-                        Items[i].SellIn = Items[i].SellIn - 1;
-
                         if (Items[i].Quality < 50)
                         {
                             Items[i].Quality = Items[i].Quality + 1;
@@ -59,8 +61,6 @@ namespace csharpcore
                         }
                         break;
                     default:
-                        Items[i].SellIn = Items[i].SellIn - 1;
-                        
                         if (Items[i].Quality > 0)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
@@ -69,7 +69,6 @@ namespace csharpcore
                                 Items[i].Quality = Items[i].Quality - 1;
                             }
                         }
-
                         break;
                 }               
             }
